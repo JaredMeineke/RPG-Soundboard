1bbea7ce154e58d4352a900d8c8752688e4ffdcd
Jared Meineke <jaredmeineke@gmail.com>
2019-05-02 19:12:17 -0400

    hcmghm

--- script/js.js
+++ script/js.js
@@ -1,11 +1,11 @@
 function getVideo(video) {
-  document.getElementById("videoPlayer").src = "https://www.youtube.com/embed/" + video + "?autoplay=1&mute=2";
+  document.getElementById("videoPlayer").src = "https://www.youtube.com/embed/" + video + "?autoplay=1&loop=1";
 }
 
 function getAudio(audio) {
-  document.getElementById("audioPlayer").src = "https://www.youtube.com/embed/" + audio + "?autoplay=1&mute=2";
+  document.getElementById("audioPlayer").src = "https://www.youtube.com/embed/" + audio + "?autoplay=1";
 }
 
 function getAmbience(ambience) {
-  document.getElementById("ambiencePlayer").src = "https://www.youtube.com/embed/" + ambience + "?autoplay=1&mute=2";
+  document.getElementById("ambiencePlayer").src = "https://www.youtube.com/embed/" + ambience + "?autoplay=1&loop=1";
 }
